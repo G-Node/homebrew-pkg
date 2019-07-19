718497152b568cabc5e6be7dbc442c8b508c1931
Achilleas Koutsou <achilleas.k@gmail.com>
2019-07-19 15:24:32 +0200

    [gin-cli] Change homepage URL: no web. subdomain

--- Formula/gin-cli.rb
+++ Formula/gin-cli.rb
@@ -1,6 +1,6 @@
 class GinCli < Formula
   desc "Command line client for the G-Node Infrastructure (GIN) services"
-  homepage "https://web.gin.g-node.org/G-Node/Info/wiki/GinCliSetup"
+  homepage "https://gin.g-node.org/G-Node/Info/wiki/GinCliSetup"
   url "https://github.com/G-Node/gin-cli/releases/download/v1.9/gin-cli-1.9-macos-amd64.tar.gz"
   sha256 "5ff7c9d87739f9e556d864120577631333cc8e0656c2b8b0c7744310cabd7f5c"
   head "https://github.com/G-Node/gin-cli.git"
