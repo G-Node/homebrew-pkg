19587067bf10b745cb8dec0a88f06cf7569412c3
Achilleas Koutsou <achilleas.k@gmail.com>
2020-03-06 14:25:56 +0100

    Fix gin-cli archive hash (v1.10)

--- Formula/gin-cli.rb
+++ Formula/gin-cli.rb
@@ -2,7 +2,7 @@ class GinCli < Formula
   desc "Command line client for the G-Node Infrastructure (GIN) services"
   homepage "https://gin.g-node.org/G-Node/Info/wiki/GinCliSetup"
   url "https://github.com/G-Node/gin-cli/releases/download/v1.10/gin-cli-1.10-macos.tar.gz"
-  sha256 "5ff7c9d87739f9e556d864120577631333cc8e0656c2b8b0c7744310cabd7f5c"
+  sha256 "97b4dff524323cb786c35e7601392674d8e5edc9f322aec87f4880347b0a4625"
   head "https://github.com/G-Node/gin-cli.git"
   version "1.10"
   version_scheme 1
