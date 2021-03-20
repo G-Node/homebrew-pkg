70cd102e88f7f9b65124f8bcbed18f6da147cfac
Michael Sonntag <michael.p.sonntag@gmail.com>
2021-03-20 12:51:39 +0100

    gin-cli release v1.12 update

--- Formula/gin-cli.rb
+++ Formula/gin-cli.rb
@@ -1,10 +1,10 @@
 class GinCli < Formula
   desc "Command line client for the G-Node Infrastructure (GIN) services"
   homepage "https://gin.g-node.org/G-Node/Info/wiki/GinCliSetup"
-  url "https://github.com/G-Node/gin-cli/releases/download/v1.10/gin-cli-1.10-macos.tar.gz"
-  sha256 "97b4dff524323cb786c35e7601392674d8e5edc9f322aec87f4880347b0a4625"
+  url "https://github.com/G-Node/gin-cli/releases/download/v1.12/gin-cli-1.12-macos.tar.gz"
+  sha256 "0d93efb3fca8bfe93e33fcb0422a6aa47bab7e89c897cb7f9453c84fb398b338"
   head "https://github.com/G-Node/gin-cli.git"
-  version "1.10"
+  version "1.12"
   version_scheme 1
 
   bottle :unneeded
