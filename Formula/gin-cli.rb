6523740ef5846c3faaac3e34b2fae96404282a31
Jan Grewe <jan.grewe@g-node.org>
2022-05-08 16:19:49 +0200

    [gin] fixes

--- Formula/gin-cli.rb
+++ Formula/gin-cli.rb
@@ -1,18 +1,14 @@
 class GinCli < Formula
-  desc "Command line client for the G-Node Infrastructure (GIN) services"
+  desc "Command-line client for the G-Node Infrastructure (GIN) services"
   homepage "https://gin.g-node.org/G-Node/Info/wiki/GinCliSetup"
   url "https://github.com/G-Node/gin-cli/releases/download/v1.12/gin-cli-1.12-macos.tar.gz"
   sha256 "0d93efb3fca8bfe93e33fcb0422a6aa47bab7e89c897cb7f9453c84fb398b338"
-  head "https://github.com/G-Node/gin-cli.git"
-  version "1.12"
   version_scheme 1
-
-  bottle :unneeded
+  head "https://github.com/G-Node/gin-cli.git"
 
   depends_on "git-annex"
 
   def install
     bin.install "gin"
   end
-
 end
